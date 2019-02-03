4fe72d58b074f947741dc43600285bda4353889a
benestruv <benedicte.struvay@gmail.com>
2019-02-03 18:21:44 +0100

    fix package-lock.json

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -27155,7 +27155,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42567" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40451" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
